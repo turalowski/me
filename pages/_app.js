76238170717997f8e4788d0870c22ad4e3c81431
Tural Hajiyev <thajiyev9@outlook.com>
2022-10-01 19:19:54 +0200

    Fresh

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,6 @@
 import '../styles/globals.css';
-import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    const threeScript = document.createElement('script');
-    threeScript.setAttribute('id', 'threeScript');
-    threeScript.setAttribute(
-      'src',
-      'https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js'
-    );
-    document.getElementsByTagName('head')[0].appendChild(threeScript);
-
-    const waves = document.createElement('script');
-    waves.setAttribute('id', 'waves');
-    waves.setAttribute(
-      'src',
-      'https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.waves.min.js'
-    );
-    document.getElementsByTagName('head')[0].appendChild(waves);
-    return () => {
-      if (threeScript) {
-        threeScript.remove();
-        waves.remove();
-      }
-    };
-  }, []);
   return <Component {...pageProps} />;
 }
 
--- utils/constants.js
+++ utils/constants.js
@@ -1,37 +1,11 @@
-import {
-  BsFacebook,
-  BsTwitter,
-  BsGithub,
-  BsLinkedin,
-  BsInstagram,
-} from 'react-icons/bs';
+import { BsTwitter, BsGithub } from 'react-icons/bs';
 
-const FACEBOOK = 'https://www.facebook.com/turalowski';
-const INSTAGRAM = 'https://www.instagram.com/turalowski/';
-const LINKEDIN = 'https://www.linkedin.com/in/tural-hajiyev-703a15155/';
 const GITHUB = 'https://github.com/turalowski';
-const TWITCH = 'https://www.twitch.tv/turalhj';
 const TWITTER = 'https://twitter.com/turalowski';
-const YOUTUBE = 'https://www.youtube.com/channel/UCHZjSTNSK7YVPV7BzdgDlbQ';
 
 export const socials = [
   {
-    name: 'Facebook',
-    link: FACEBOOK,
-    icon: props => <BsFacebook {...props} />,
-  },
-  {
-    name: 'Instagram',
-    link: INSTAGRAM,
-    icon: props => <BsInstagram {...props} />,
-  },
-  {
-    name: 'LinkedIn',
-    link: LINKEDIN,
-    icon: props => <BsLinkedin {...props} />,
-  },
-  {
-    name: 'Twitter',
+    name: 'Github',
     link: GITHUB,
     icon: props => <BsGithub {...props} />,
   },
